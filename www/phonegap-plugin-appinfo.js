1a903bc48000655e8e86a445d5ebd7491169d1ae
kennethyip <kenneth.yip@tungwah.org.hk>
2014-03-31 11:12:56 +0800

    Revert "Edit"
    This reverts commit 1363b75463e7db308555f1c3e6c1cd394857e67b.

--- www/phonegap-plugin-appinfo.js
+++ www/phonegap-plugin-appinfo.js
@@ -1,7 +1,7 @@
 var appInfo_exec = require('cordova/exec');
-window.getAppVersionName = function (callback, errCallbac) {
+exports.getAppVersionName = function (callback, errCallbac) {
     appInfo_exec(callback, errCallbac, "phonegap-plugin-appinfo", "getAppVersionName", []);
 };
-window.getAppVersionCode = function (callback, errCallbac) {
+exports.getAppVersionCode = function (callback, errCallbac) {
     appInfo_exec(callback, errCallbac, "phonegap-plugin-appinfo", "getAppVersionCode", []);
 };
